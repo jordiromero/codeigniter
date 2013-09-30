70acd26ef707654f7df526135f6e83186b1d9d1c
jordiromero <jordiromero@iesebre.com>
2013-09-30 20:00:24 +0200

    jquery

--- application/views/create_table.php
+++ application/views/create_table.php
@@ -1,25 +1,39 @@
 <html>
 	<head>
+		<style type="text/css" title="currentStyle">
+	
+	</style>
 	<!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
+<!--<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">-->
  
 <!-- jQuery -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<!--<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script> -->
  
 <!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<!--<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script> -->
+<script type="text/javascript" language="javascript" src="/usr/share/javascript/jquery/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="/usr/share/javascript/jquery/datatables/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf-8">
+<script src="/usr/share/javascript/jquery/datatables/jquery.dataTables.js">	
+$(document).ready(function() {
+ $('#example').dataTable();
+} );
+</script> 
 	
 	
 	</head>
 	
 	<body>
-	<table id="usuaris" border="1">
+	<table id="exemple" border="1" class="display"  width="100%">
+	<thead>	
 
 	<tr>
 	<th> Text de cap&ccedil;alera </th>
 	<th> Text de cap&ccedil;alera </th>
 	<th> Text de cap&ccedil;alera</th>
 	</tr>
+	</thead>
+	<tbody>
 	<tr>
 	<td> Text de cel&middot;la </td>
 	<td> Text de cel&middot;la </td>
@@ -35,6 +49,7 @@
 	<td> Text de cel&middot;la </td>
 	<td> Text de cel&middot;la</td>
 	</tr>
+	</tbody>
 </table>
 	
 	</body>
