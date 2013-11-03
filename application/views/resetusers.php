17cbfaf66095ce79c4a3ebbb40d04b99b1294f9d
jordiromero <jordiromero@iesebre.com>
2013-11-03 18:29:27 +0100

    03112013

---
+++ application/views/resetusers.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#" class="navbar-brand">Modifica la teva contrasenya</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="create">Registrat</a></li>
+            <li><a href="modify">Modifica el teu usuari</a></li>
+            <li><a href="/codeigniterhelloworld/application/views/inici.php">Menu Principal</a></li>
+          </ul>
+        </div>
+      </div>
+      </div>
+<div>
+</div>
+</br>
+</br>
+</br>
+</br>
+</body>
+</html>
