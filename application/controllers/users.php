152c81dad9ac52156e881237d62089c572c8ce9b
jordiromero <jordiromero@iesebre.com>
2013-11-03 18:15:50 +0100

    users.php

---
+++ application/controllers/users.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+		
+		$this->load->model('user_model');
+	}
+	
+	public function create(){
+		
+		$this->load->view('create');
+	}
+	public function delete($id) {
+		
+		$this->load->view('delete');
+	}
+	public function modify($id){
+		
+		$this->load->view('modify');
+	}
+	public function listusers(){
+		$data['hello'] = $this->User_model->hello();
+		$this->load->view('listusers', $data);
+	}
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
