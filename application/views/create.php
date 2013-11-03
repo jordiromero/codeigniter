21e84a05d786aa1eed7239d5ee8298b21d92dbb7
jordiromero <jordiromero@iesebre.com>
2013-11-03 18:18:23 +0100

    Update create.php

--- application/views/create.php
+++ application/views/create.php
@@ -1,5 +1,40 @@
 <html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+</head>
 <body>
-<h1>Aqui pots crear el teu usuari</h1>
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#" class="navbar-brand">Crea el teu usuari</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="resetusers">Canvia la teva contrasenya</a></li>
+            <li><a href="modify">Modifica el teu usuari</a></li>
+            <li><a href="/codeigniterhelloworld/application/views/inici.php">Menu Principal</a></li>
+          </ul>
+        </div>
+      </div>
+      </div>
+<div>
+</div>
+</br>
+</br>
+</br>
+</br>
+</div>
 </body>
 </html>
